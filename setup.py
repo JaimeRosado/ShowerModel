939db7e71967bc98264da3cf82570771d46b7d10
Daniel Morcuende <dmorcuen@ucm.es>
2020-11-30 17:50:30 +0100

    add setup.py to project

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+docs_require = [
+    "sphinx_rtd_theme",
+    "sphinx_automodapi",
+    "sphinx",
+    "nbsphinx",
+    "numpydoc",
+    "jupyter",
+    "notebook",
+    "graphviz",
+]
+
+setup(
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scipy"
+    ],
+    name='ShowerModel',
+    version='0.1.0',
+    url='https://github.com/JaimeRosado/ShowerModel',
+    license='GPL-3.0',
+    author='Jaime Rosado',
+    author_email='jrosadov@ucm.es',
+    description='Modelling cosmic-ray showers, their light production and its detection.',
+    # here are optional dependencies (as "tag" : "dependency spec")
+    extras_require={"docs": docs_require},
+)
