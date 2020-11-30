a3321fc79b8b7430cb60af4678d0635fa256f122
Daniel Morcuende <dmorcuen@ucm.es>
2020-11-30 13:19:59 +0100

    remove creation of html file artifact

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'ShowerModel'
-copyright = '2020, Daniel Morcuende'
-author = 'Daniel Morcuende'
+copyright = '2020, Jaime Rosado'
+author = 'Jaime Rosado'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
@@ -54,3 +54,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Refer figures
+numfig = True
