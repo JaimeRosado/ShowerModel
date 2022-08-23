7d87c861f32f2b19f84640d2bf15ecf4bc467a8c
Jaime Rosado <jaime_ros@fis.ucm.es>
2022-08-23 16:20:36 +0200

    Add some tests

--- showermodel/tests/test_shower.py
+++ showermodel/tests/test_shower.py
@@ -3,7 +3,11 @@ def test_shower():
     theta = 20.0
     # Check the value obtained with the settings below is the following
     h_top = 112.8292
-    shower = Shower(1.e6, theta=theta, az=45.0, x0=0.1, y0=0.2)
+    shower = Shower(1.e6, theta=theta, az=45.0, x0=0.1, y0=0.2, atm_model=17)
     assert shower.alt == 90.0 - theta
     assert shower.az == 45.0
     assert shower.h_top == h_top
+    assert shower.atmosphere.P[100] == 49.69842905334434
+    assert shower.track.t[200] == 254.50685251195043
+    assert shower.profile.s[10] == 1.6504072224436146
+    assert shower.cherenkov.N_ph[50] == 757147.620313149
