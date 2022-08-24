32a7e6e2aa76a79fa184dd831a8b07b73f70c91b
Daniel Morcuende <dmorcuen@ucm.es>
2022-08-24 11:09:53 +0200

    update test for Telescope class with default parameters

--- showermodel/tests/test_telescope.py
+++ showermodel/tests/test_telescope.py
@@ -1,11 +1,10 @@
 def test_telescope():
-    from showermodel import IACT
+    from showermodel.telescope import Telescope
 
     # Default telescope type: IACT
     # Default angular aperture in degrees: 8.0
     # Default detection area in m2: 113.097
-    telescope = IACT()
-    assert telescope.tel_type == 'IACT'
-    assert telescope.apert == 8.0
-    assert telescope.area == 113.097
-
+    telescope = Telescope()
+    assert telescope.tel_type == 'generic'
+    assert telescope.apert == 10.0
+    assert telescope.area == 100.0
